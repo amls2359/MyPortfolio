5e3c37795c20000413571d08438c82d29caeb34a
Amal <amalskumr20@gmail.com>
2025-09-10 22:41:25 +0530

    add router

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,6 @@
 const express = require('express');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const User = require('../models/User');
 const nodemailer = require('nodemailer');
+const router = express.Router();
 
 require('dotenv').config();
 
