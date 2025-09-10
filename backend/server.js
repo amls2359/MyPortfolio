1e959d161411ea8b8417d6cb248ab44b6092f0a3
Amal <amalskumr20@gmail.com>
2025-09-10 22:56:54 +0530

    Serve React build folder from backend

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,7 @@
 const express= require('express')
 const app = express()
-const mongoose = require('mongoose')
 const cors = require('cors')
-const jwt = require('jsonwebtoken')
+const path = require('path')
 require('dotenv').config()
 
 const authRoutes = require('./routes/authRoutes')
@@ -12,12 +11,13 @@ const authRoutes = require('./routes/authRoutes')
 
 app.use(cors())
 app.use (express.json())
-
-// mongoose.connect(process.env.MONGO_URI)
-//   .then(() => console.log('MongoDB connected'))
-//   .catch(err => console.error('MongoDB connection error:', err));
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 app.use('/', authRoutes)
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000; 
 app.listen (PORT ,() => {
