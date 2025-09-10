3983a756ece15e6d513cbda9f16e6feff8228e51
Amal <amalskumr20@gmail.com>
2025-09-10 22:32:00 +0530

    Removed MongoDB connection and updated routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -6,50 +6,6 @@ const nodemailer = require('nodemailer');
 
 require('dotenv').config();
 
-
-const router = express.Router();
-
-router.post('/signup',async(req,res)=>{
-    const {name,email,password,phoneNumber}= req.body
-    try
-    {
-        const existingUser = await User.findOne({email})
-        if(existingUser)
-        {
-            return res.status(400).json({message: "User already exists"})
-        }
-
-        const hashedPassword = await bcrypt.hash(password,10)
-        const newUser = new User({name,email,password:hashedPassword,phoneNumber})
-        await newUser.save()
-
-        res.json({message:'signup successfull'})
-    }
-    catch(error)
-    {
-       res.status(500).json({message:'Internal server error'})
-    }
-
-})
-
-router.post('/login', async (req, res) => {
-  const { email, password } = req.body;
-
-  const user = await User.findOne({ email });
-  if (!user) return res.status(400).json({ message: 'User not found' });
-
-
-   const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-      return res.status(400).json({ message: 'Invalid email or password' });
-    }
-
-  const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
-  
-    res.json({ message: 'Login successful', token });
-
-});
-
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
--- backend/server.js
+++ backend/server.js
@@ -13,12 +13,13 @@ const authRoutes = require('./routes/authRoutes')
 app.use(cors())
 app.use (express.json())
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
+// mongoose.connect(process.env.MONGO_URI)
+//   .then(() => console.log('MongoDB connected'))
+//   .catch(err => console.error('MongoDB connection error:', err));
 
 app.use('/', authRoutes)
 
-app.listen (process.env.PORT ,() => {
+const PORT = process.env.PORT || 5000; 
+app.listen (PORT ,() => {
     console.log(`Server is running on port ${process.env.PORT}`)
 })
