26eabb6b1d2cbbb97ab3db916de099f52fd6489f
Amal <amalskumr20@gmail.com>
2025-09-10 23:09:06 +0530

    Fix wildcard route with safe regex

--- backend/server.js
+++ backend/server.js
@@ -17,9 +17,10 @@ app.use('/', authRoutes)
 
 const frontendPath = path.join(__dirname, '../frontend/build');
 
-app.get('/*', (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(frontendPath, 'index.html'));
 });
+
 
 const PORT = process.env.PORT || 5000; 
 app.listen (PORT ,() => {
